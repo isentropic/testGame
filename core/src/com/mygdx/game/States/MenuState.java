bb0d39c17e36d3cdb79b4c6e31fb6db741f9ed5e
Dauren Baitursyn <biddy.as.diddy@gmail.com>
2017-03-21 15:49:37 +0900

    Added MenuState and GameStateManager

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,29 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by biddy on 3/21/17.
+ */
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sp) {
+
+    }
+}
