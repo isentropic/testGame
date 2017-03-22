f94af972439e5964046bd34ee511715529d7f578
Dauren Baitursyn <biddy.as.diddy@gmail.com>
2017-03-22 20:54:48 +0900

    Added Tube

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,10 +1,12 @@
 package com.mygdx.game.states;
 
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
 
 /**
  * Created by biddy on 3/22/17.
@@ -13,16 +15,22 @@ import com.mygdx.game.sprites.Bird;
 public class PlayState extends State {
 
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
+        tube = new Tube(100);
+        bg = new Texture("bg.png");
         cam.setToOrtho(false, MyGdxGame.WIDTH/2, MyGdxGame.HEIGHT/2);
     }
 
     @Override
     public void handleInput() {
-
+        if (Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
@@ -35,7 +43,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sp) {
         sp.setProjectionMatrix(cam.combined);
         sp.begin();
+        sp.draw(bg, cam.position.x - (cam.viewportWidth/2), 0);
         sp.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sp.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sp.draw(tube.getBotTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sp.end();
     }
 
