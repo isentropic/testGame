8f3c08b7a4acdd31fd239b5f767ef24f796a7877
Dauren Baitursyn <biddy.as.diddy@gmail.com>
2017-03-22 20:23:37 +0900

    Added Bird and gravity

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -4,6 +4,7 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.sprites.Bird;
 
 /**
  * Created by biddy on 3/22/17.
@@ -11,11 +12,11 @@ import com.mygdx.game.MyGdxGame;
 
 public class PlayState extends State {
 
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
         cam.setToOrtho(false, MyGdxGame.WIDTH/2, MyGdxGame.HEIGHT/2);
     }
 
@@ -26,14 +27,15 @@ public class PlayState extends State {
 
     @Override
     public void update(float deltaTime) {
-
+        handleInput();
+        bird.update(deltaTime);
     }
 
     @Override
     public void render(SpriteBatch sp) {
         sp.setProjectionMatrix(cam.combined);
         sp.begin();
-        sp.draw(bird, 50, 50);
+        sp.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sp.end();
     }
 
